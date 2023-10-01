1c6792774bd3e3cd8d87a629340c04deab55f06b
Anvar <anvar.ganiev99@gmail.com>
2023-10-01 13:15:38 -0400

    fixed navbar

--- react-portfolio/src/components/navbar.js
+++ react-portfolio/src/components/navbar.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+//importing styles
+import '../styles/navbar.css'
+
 function navbar() {
   return (
     <div className="navbar">
-        <div className="toggleButton">
-            <button>
-
-            </button>
-            <div classname = "links">
-                <Link to="/">Home</Link>
-                <Link to="/resume">Resume</Link>
-                <Link to="/portfolio">Portfolio</Link>
-                <Link to="/contacts">Contacts</Link>
-            </div>
+      <h1 className="name">Anvar Ganiev</h1>
+      <div className="toggleButton">
+        <button></button>
+      </div>
+        <div className="links">
+          <Link to="/">About Me</Link>
+          <Link to="/resume">Resume</Link>
+          <Link to="/portfolio">Portfolio</Link>
+          <Link to="/contacts">Contacts</Link>
         </div>
     </div>
   )
