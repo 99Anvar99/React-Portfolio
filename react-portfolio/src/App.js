bf17907800b1336626fadc3dfcb4c8eae51424f5
Anvar <anvar.ganiev99@gmail.com>
2023-10-01 10:41:16 -0400

    moved .css into styles folder

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Importing pages
 import AboutMe from './pages/aboutMe';
