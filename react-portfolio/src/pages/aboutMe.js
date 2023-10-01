6ec44567f0cb72e18e7386cfc4fc1b8cd6dbff5e
Anvar <anvar.ganiev99@gmail.com>
2023-10-01 10:35:18 -0400

    added pages

---
+++ react-portfolio/src/pages/aboutMe.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function aboutMe() {
+  return (
+    <div>aboutMe</div>
+  )
+}
+
+export default aboutMe
---
+++ react-portfolio/src/pages/resume.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function resume() {
+  return (
+    <div>resume</div>
+  )
+}
+
+export default resume
