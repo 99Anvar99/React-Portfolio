cee4e9e5383f31362559f8c740d1d03b4feee71e
Anvar <anvar.ganiev99@gmail.com>
2023-10-01 10:35:41 -0400

    imported pages and components

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// Importing pages
+import AboutMe from './pages/aboutMe';
+import Resume from './pages/resume';
+import Portfolio from './pages/portfolio';
+import Contacts from './pages/contacts';
+// Improting components
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<AboutMe />} />
+        <Route path='/resume' element={<Resume />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+        <Route path='/contacts' element={<Contacts />} />
+      </Routes>
+     </Router>
     </div>
   );
 }
