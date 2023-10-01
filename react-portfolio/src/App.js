f6d5fff42a0a8acc916460e15cdcd7caa943f95d
Anvar <anvar.ganiev99@gmail.com>
2023-10-01 13:15:48 -0400

    importing footer

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -7,6 +7,7 @@ import Portfolio from './pages/portfolio';
 import Contacts from './pages/contacts';
 // Improting components
 import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path='/portfolio' element={<Portfolio />} />
         <Route path='/contacts' element={<Contacts />} />
       </Routes>
+      <Footer />
      </Router>
     </div>
   );
