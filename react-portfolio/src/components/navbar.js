48ef8f5e0d69a5f8cb366ba79e7963e5e19cbdc8
Anvar <anvar.ganiev99@gmail.com>
2023-10-02 13:56:37 -0400

    active page is now highlighted

--- react-portfolio/src/components/navbar.js
+++ react-portfolio/src/components/navbar.js
@@ -1,23 +1,55 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-//importing styles
-import '../styles/navbar.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+// importing styles
+import '../styles/navbar.css';
 
-function navbar() {
+function Navbar() {
+  // Define a state variable to track the active page
+  const [activePage, setActivePage] = useState('');
+
+  // Function to set the active page based on the current location
+  const setActive = (path) => {
+    setActivePage(path);
+  };
+
   return (
     <div className="navbar">
       <h1 className="name">Anvar Ganiev</h1>
       <div className="toggleButton">
         <button></button>
       </div>
-        <div className="links">
-          <Link to="/">About Me</Link>
-          <Link to="/portfolio">Portfolio</Link>
-          <Link to="/contacts">Contacts</Link>
-          <Link to="/resume">Resume</Link>
-        </div>
+      <div className="links">
+        <Link
+          to="/"
+          onClick={() => setActive('/')}
+          className={activePage === '/' ? 'active' : ''}
+        >
+          About Me
+        </Link>
+        <Link
+          to="/portfolio"
+          onClick={() => setActive('/portfolio')}
+          className={activePage === '/portfolio' ? 'active' : ''}
+        >
+          Portfolio
+        </Link>
+        <Link
+          to="/contacts"
+          onClick={() => setActive('/contacts')}
+          className={activePage === '/contacts' ? 'active' : ''}
+        >
+          Contacts
+        </Link>
+        <Link
+          to="/resume"
+          onClick={() => setActive('/resume')}
+          className={activePage === '/resume' ? 'active' : ''}
+        >
+          Resume
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default navbar
+export default Navbar;
